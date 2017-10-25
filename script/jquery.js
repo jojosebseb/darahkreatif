f6b6622dc995035c0b5371fe901fd7e14af88b45
Jonathan Sebastian <jojosebseb@gmail.com>
2017-10-25 10:48:52 +0700

    v1

--- script/jquery.js
+++ script/jquery.js
@@ -6,31 +6,63 @@ $('.blue-button').on('click', function(){
     int = parseInt(lastChar)+1;
     $(this).parents('.progress-section').removeClass('active');
     $('#section-'+int).addClass('active');
-
+    if ($(this).attr('id') == 'Finish') {
+        $(this).parents('.progress-section').removeClass('active');
+        $('#section-8').addClass('active');
+    }
+});
+$('#shortCut').on('click', function(){
+    $(this).parents('.progress-section').removeClass('active');
+    $('#section-shortCut').addClass('active');
 });
 
 $('.progress-section').each(function(){
     $(this).find('*').each(function(i){
         $(this).css({
-            'transition-delay': i/50+'s'
+            'transition-delay': i/10+'s'
+        });
+    });
+    $(this).find('p').each(function(i){
+        $(this).css({
+            // 'transition-delay': (i/1)+0.50+'s'
         });
     });
 });
 
-$('.droppable-parent').sortable();
+// $('.droppable-parent').sortable();
 // $('.droppable-parent').draggable();
 
-$('.jo-checkbox').each(function(){
 
-});
 
-setInterval(function(){
+setTimeout(function(){
     $('.preload-parent').addClass('hide');
     $('.preload-parent-2').addClass('active');
 }, 2000);
 
-setInterval(function(){
+setTimeout(function(){
     $('.progressive-parent').addClass('active');
+    $('#section-1').addClass('active');
+
     $('.logo').addClass('active');
     $('.preload-parent-2').addClass('hide');
 }, 4100);
+
+
+$('.jo-checkbox').each(function(){
+    $(this).wrap('<div class="jo-checkbox-parent"><div class="jo-checkbox-box"></div></div>');
+});
+
+$('body').on('click', '.jo-checkbox-parent', function(){
+    $(this).toggleClass('active');
+    if ($(this).hasClass('active')) {
+        $(this).find('.jo-checkbox').attr('checked', true);
+    }
+    else {
+        $(this).find('.jo-checkbox').attr('checked', false);
+    }
+    // console.log('asd');
+});
+
+$('.jo-checkbox').on('change', function(){
+    console.log('asd');
+})
